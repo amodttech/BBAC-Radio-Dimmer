3388eacbc39c20f28172bb35ac7308c565f67028
Aaron Amodt <aaron@amodttech.com>
2022-03-11 16:02:29 -0500

    refactored, and commented

--- BBAC-DMX-Radio-Dimmer.ino
+++ BBAC-DMX-Radio-Dimmer.ino
@@ -1,16 +1,26 @@
+// Include library for DMX Shield
 #include <Conceptinetics.h>
+
+// The slave device will use a block of 10 channels, counting from its start address.
 #define DMX_SLAVE_CHANNELS   10 
+// Configure a DMX slave controller
 DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
 
-const int ledPin = 9;
+// Using pin 9 for PWM, required for dimming
+int ledPin = 9;
 
 void setup() {
-  dmx_slave.enable();  
-  dmx_slave.setStartAddress(450);
-  pinMode(ledPin, OUTPUT);
+    // Enable DMX slave and start recording DMX data
+    dmx_slave.enable();
+    // Set starting address
+    dmx_slave.setStartAddress(450);
+    // Set ledPin as an OUTPUT pin
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  int dmxBrightness = dmx_slave.getChannelValue(1);
-  analogWrite(ledPin, dmxBrightness);
+    // Create brightness variable that is defined as the DMX signal value
+    int dmxBrightness = dmx_slave.getChannelValue(1);
+    // Output brightness level to the LED.
+    analogWrite(ledPin, dmxBrightness);
   }
